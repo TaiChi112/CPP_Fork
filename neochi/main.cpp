df2a34ffc1fe04eef87b6531fe0e20bee5996742
CBTaiChi112 <github.taichi@gmail.com>
2025-01-30 18:01:25 +0000

    chore : nothing much I just try dev a little bit

---
+++ neochi/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int something(112);
+    cout << "Input something in the variable something : ";
+    cin >> something;
+    cout << "Output result value in the variable something : " << something << endl;
+    return 0;
+}
