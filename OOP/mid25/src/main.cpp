493319789e0041ba77c9a1f8cdc326bb2a6de689
TaiChi <anothai.0978452316@gmail.com>
2025-05-06 01:23:08 +0700

    feat: new class & implement & test a little bit

--- OOP/mid25/src/main.cpp
+++ OOP/mid25/src/main.cpp
@@ -7,9 +7,39 @@ enum class E_Stove_Type
     Electric,
     Induction
 };
+class Stove
+{
+    E_Stove_Type type;
 
+public:
+    Stove(E_Stove_Type t) : type(t) {}
+    void set_stove_type(E_Stove_Type t) { type = t; }
+    E_Stove_Type get_stove_type() { return type; }
+    void show_stove_type()
+    {
+        switch (type)
+        {
+        case E_Stove_Type::Gas:
+            cout << "Gas Stove" << endl;
+            break;
+        case E_Stove_Type::Electric:
+            cout << "Electric Stove" << endl;
+            break;
+        case E_Stove_Type::Induction:
+            cout << "Induction Stove" << endl;
+            break;
+        }
+    }
+};
 int main()
 {
-    
+    Stove stove(E_Stove_Type::Gas);
+    stove.show_stove_type(); 
+
+    Stove array_stove[3] = {E_Stove_Type::Gas, E_Stove_Type::Electric, E_Stove_Type::Induction};
+    for (int i = 0; i < 3; i++)
+    {
+        array_stove[i].show_stove_type();
+    }
     return 0;
 }
