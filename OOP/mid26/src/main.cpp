a340e9e13034db9a8395fedbbc6bfa50c67a53f7
TaiChi <anothai.0978452316@gmail.com>
2025-05-08 22:08:15 +0700

    feat: success all object after adding condition

--- OOP/mid26/src/main.cpp
+++ OOP/mid26/src/main.cpp
@@ -21,14 +21,27 @@ enum class e_pants_type
     CROPPED_PANTS,
     TROUSERS,
 };
+
+class s_size
+{
+    int width, lenght, height;
+
+public:
+    s_size() : width(60), lenght(80), height(180) {}
+    s_size(int width, int lenght, int height) : width(width), lenght(lenght), height(height) {}
+    int get_width() { return width; }
+    int get_lenght() { return lenght; }
+    int get_height() { return height; }
+};
 class Clothes
 {
     double price;
     e_clothes_size size;
 
 public:
-    Clothes() : price(0.0), size(e_clothes_size::SMALL) {}
+    Clothes() : price(99.00), size(e_clothes_size::MEDIUM) {}
     Clothes(double price, e_clothes_size size) : price(price), size(size) {}
+    void set_price(double price) { this->price = price; }
     void show_clothes_info()
     {
         cout << "Price: " << price << endl;
@@ -54,11 +67,12 @@ class Tops : public Clothes
     e_tops_type type;
 
 public:
-    Tops() : Clothes(), type(e_tops_type::T_SHIRT) {}
+    Tops() : Clothes(199.00, e_clothes_size::MEDIUM), type(e_tops_type::T_SHIRT) {}
     Tops(double price, e_clothes_size size, e_tops_type type) : Clothes(price, size), type(type) {}
+    
     void show_tops_info()
     {
-        show_clothes_info();
+        Clothes::show_clothes_info();
         switch (type)
         {
         case e_tops_type::T_SHIRT:
@@ -75,14 +89,15 @@ public:
 };
 class Pants : public Clothes
 {
-    e_pants_type type;
+    e_pants_type lenght;
+
 public:
-    Pants() : Clothes(), type(e_pants_type::SHORTS) {}
-    Pants(double price, e_clothes_size size, e_pants_type type) : Clothes(price, size), type(type) {}
+    Pants() : Clothes(259.00, e_clothes_size::LARGE), lenght(e_pants_type::SHORTS) {}
+    Pants(double price, e_clothes_size size, e_pants_type lenght) : Clothes(price, size), lenght(lenght) {}
     void show_pants_info()
     {
-        show_clothes_info();
-        switch (type)
+        Clothes::show_clothes_info();
+        switch (lenght)
         {
         case e_pants_type::SHORTS:
             cout << "Type: Shorts" << endl;
@@ -99,7 +114,41 @@ public:
         }
     }
 };
+class Wardrobe
+{
+    s_size size;
+    Tops *tops;
+    int amount_of_tops;
+    Pants *pants;
+    int amount_of_pants;
+
+public:
+    Wardrobe() : size(s_size(60, 80, 180)), tops(new Tops), pants(new Pants()) {}
+    Wardrobe(s_size size, Tops *tops, int amount_of_tops, Pants *pants, int amount_of_pants) : size(size), tops(tops), amount_of_tops(amount_of_tops), pants(pants), amount_of_pants(amount_of_pants) {}
+    void show_wardrobe_info()
+    {
+        cout << "Wardrobe size: " << size.get_width() << "x" << size.get_lenght() << "x" << size.get_height() << endl;
+        cout << "Tops:" << endl;
+        for (int i = 0; i < amount_of_tops; i++)
+        {
+            tops[i].show_tops_info();
+        }
+        cout << endl;
+        cout << "Pants:" << endl;
+        for (int i = 0; i < amount_of_pants; i++)
+        {
+            pants[i].show_pants_info();
+        }
+    }
+};
 int main()
 {
+    Wardrobe wardrobe(
+        s_size(60, 80, 180),
+        new Tops[2]{Tops(199.00, e_clothes_size::MEDIUM, e_tops_type::T_SHIRT), Tops(299.00, e_clothes_size::LARGE, e_tops_type::SHIRT)},
+        2,
+        new Pants[2]{Pants(259.00, e_clothes_size::LARGE, e_pants_type::SHORTS), Pants(359.00, e_clothes_size::XLARGE, e_pants_type::TROUSERS)},
+        2);
+    wardrobe.show_wardrobe_info();
     return 0;
 }
