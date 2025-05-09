f840356779b7f04510c8ca7ab5cb76e38b1d4948
TaiChi <anothai.0978452316@gmail.com>
2025-05-09 21:49:03 +0700

    rfc: impl abstract inherit class & impl fn a littiel bit

--- OOP/final65/src/main.cpp
+++ OOP/final65/src/main.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 
 using namespace std;
-enum class e_kao_niew_color{
+enum class e_kaoniew_color{
     WHITE,
     BLACK
 };
@@ -12,32 +12,63 @@ enum class e_kai_yang_type{
 class Food{
     double price;
     public:
+    Food():price(40.00){}
+    Food(double p):price(p){}
     virtual void show_food_info() = 0;
-        
+    int get_price() {return price; };
 };
 class KaoNiew : public Food{
-    e_kao_niew_color color;
+    e_kaoniew_color color;
     public:
+    KaoNiew(double p, e_kaoniew_color e) :Food(p), color(e) {};
+    void show_food_info() override {
+        cout << "Kao Niew price: " << this->get_price() << endl;
+        cout << "Kao Niew color: " << (color == e_kaoniew_color::WHITE ? "White" : "Black") << endl;
+    }
         
 };
 class KaiYang: public Food{
     e_kai_yang_type type;
     public:
+    KaiYang(double p, e_kai_yang_type e) :Food(p), type(e) {
+    };
+    void show_food_info() override {
+        cout << "Kai Yang price: " << this->get_price() << endl;
+        cout << "Kai Yang type: " << (type == e_kai_yang_type::GRILLED_CHICKEN_WITH_FRESH_MILK ? "Grilled Chicken with Fresh Milk" : "Grilled Chicken with Turmeric") << endl;
+    }
         
 };
 class SomTum: public Food{
     int amount_of_chili;
     public:
+    SomTum(double p, int amount_ch) :Food(p), amount_of_chili(amount_ch) {}
     virtual void show_somtum_info() = 0;
+    void show_food_info() override {
+        cout << "Som Tum amount of chili: " << amount_of_chili << endl;
+    }
         
 };
 class TumPu : public SomTum{
     bool is_plara;
     public:
+    TumPu(double p, int amount_ch, bool plara) :SomTum(p, amount_ch), is_plara(plara) {}
+    void show_somtum_info() override {
+        cout << "Tum Pu is plara: " << (is_plara ? "Yes" : "No") << endl;
+    }
         
 };
 class TumThai: public SomTum{
     bool is_peanut;
+    public:
+    TumThai(double p, int amount_ch, bool peanut) :SomTum(p, amount_ch), is_peanut(peanut) {}
+    void show_somtum_info() override {
+        cout << "Tum Thai is peanut: " << (is_peanut ? "Yes" : "No") << endl;
+    }
+        
+};
+class  FoodItem{
+    Food* food;
+    int amount_of_food;
     public:
         
 };
@@ -45,12 +76,6 @@ class Order {
     FoodItem** items;
     int amount_of_items;
     int queue_number;
-    public:
-        
-};
-class  FoodItem{
-    Food* food;
-    int amount_of_food;
     public:
         
 };
