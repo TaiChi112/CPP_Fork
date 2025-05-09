8e3849b8099d81b279cdddf0bac7ce1bdaef9456
TaiChi <anothai.0978452316@gmail.com>
2025-05-10 01:50:51 +0700

    feat: initial class of object

---
+++ OOP/final66/src/main.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+
+using namespace std;
+class Meat{
+    public:
+        
+};
+class Fish: public Meat{
+    public:
+        
+};
+        
+class Chicken: public Meat{
+    public:
+        
+};
+class Thai_Food{
+    public:
+        
+};
+class Soup : public Thai_Food{
+    public:
+        
+};
+class Tom_Yam: public Soup{
+    public:
+        
+};
+class Tom_Klong: public Soup{
+    public:
+        
+};
+class Larb: public Thai_Food{
+    public:
+        
+};
+int main(){
+    return 0;
+}
