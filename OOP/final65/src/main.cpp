cbe24944eb8626bafe00f75724893391b765972a
TaiChi <anothai.0978452316@gmail.com>
2025-05-09 20:34:38 +0700

    feat: defination of object

---
+++ OOP/final65/src/main.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+
+using namespace std;
+class Food{
+    public:
+        
+};
+class KaoNiew : public Food{
+    public:
+        
+};
+class KaiYang: public Food{
+    public:
+        
+};
+class SomTum: public Food{
+    public:
+        
+};
+class TumPu : public SomTum{
+    public:
+        
+};
+class TumThai: public SomTum{
+    public:
+        
+};
+class Order {
+    public:
+        
+};
+class  FoodItem{
+    public:
+        
+};
+int main(){
+    return 0;
+}
